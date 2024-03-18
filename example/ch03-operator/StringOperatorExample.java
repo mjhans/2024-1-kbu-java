2a5aaae98779cc8bd76c4b22f6c9be152113bd0b
mjhans <mjhans@krafton.com>
2024-03-18 22:10:02 +0900

    String 연결 예제 추가

---
+++ example/ch03-operator/StringOperatorExample.java
@@ -0,0 +1,14 @@
+public class StringOperatorExample {
+    public static void main(String[] args) {
+        String str1 = "JAVA" + 1.17;
+        String str2 = str1 + " 쉬워요";
+
+        System.out.println(str2); //JAVA1.17 쉬워요 가 출력
+
+        String str3 = "JAVA" + 3 + 3.0; // JAVA33.0
+        System.out.println(str3); //JAVA33.0
+
+        String str4 = 3 + 3.0 + "JAVA"; // 6.0JAVA
+        System.out.println(str4); //6.0JAVA
+    }
+}
