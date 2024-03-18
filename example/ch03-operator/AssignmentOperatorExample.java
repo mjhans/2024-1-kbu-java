61afabe9bd71ca66b266e6a11e38e40b3c65d68d
mjhans <mjhans@krafton.com>
2024-03-18 21:42:56 +0900

    연산자 내용 추가

---
+++ example/ch03-operator/AssignmentOperatorExample.java
@@ -0,0 +1,21 @@
+public class AssignmentOperatorExample {
+    public static void main(String[] args) {
+        int result = 10;
+        System.out.println("result = 10 : " + result);
+
+        result += 2;
+        System.out.println("result += 2 : " + result);
+
+        result -= 5;
+        System.out.println("result -= 5 : " + result);
+
+        result *= 5;
+        System.out.println("result *= 5 : " + result);
+
+        result /= 5;
+        System.out.println("result /= 5 : " + result);
+        
+        result %= 3;
+        System.out.println("result %= 3 : " + result);
+    }
+}
---
+++ example/ch03-operator/IsDoneExample.java
@@ -0,0 +1,10 @@
+public class IsDoneExample {
+    public static void main(String[] args) {
+        boolean isDone = true;
+        System.out.println("initialized isDone: " + isDone);
+        isDone = !isDone;
+        System.out.println("!isDone: " + isDone);
+        isDone = !isDone;
+        System.out.println("!isDone: " + isDone);
+    }
+}
