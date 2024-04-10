79c88f4942150dc5a28f39075a24978487bd602a
mjhans <mjhans@krafton.com>
2024-04-10 18:49:04 +0900

    배열 변경

--- example/ch05-array/TwoDimArrayDemo.java
+++ example/ch05-array/TwoDimArrayDemo.java
@@ -1,6 +1,6 @@
 public class TwoDimArrayDemo{
     public static void main(String[] args){
-        int iArray[][] = new int[3][5];
+        int[][] iArray = new int[3][5];
         //iArray.length는 2차원 배열의 행(row)의 수
         for (int i = 0 ; i < iArray.length ; i++){ 
             // iArray[0].length는 1행에 있는 배열의 길이
