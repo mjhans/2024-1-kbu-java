6fd94209b61c36b3b804de81237249c3156b729f
mjhans <mjhans@krafton.com>
2024-05-02 00:13:13 +0900

    객체와 클래스 내용 반영

---
+++ example/ch07-ObjectOrientedProgramming/DiceDemo.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+class Dice{
+    public int MAX_VALUE = 6;
+    public int MIN_VALUE = 1;
+
+    public int faceValue = 0;
+
+    public void roll(){
+        Random rnd = new Random();
+        faceValue = rnd.nextInt(100) % 6 + 1;
+    }
+}
+
+class DiceDemo{
+    public static void main(String[] args){
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+
+        d1.roll();
+        d2.roll();
+        System.out.printf("two dice : %d \n", d1.faceValue + d2.faceValue);
+    }
+}
